af3530e7eac664f261e8e6577ff5028ad46875f4
juicecultus <juicecultus@gmail.com>
2020-06-16 13:10:31 +0100

    add Palette Preview component

--- components/ColorBox.js
+++ components/ColorBox.js
@@ -27,6 +27,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginBottom: 10,
     padding: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.3,
+    shadowRadius: 1,
+    elevation: 2,
   },
   boxText: {
     fontWeight: 'bold',
